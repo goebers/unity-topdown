ae4e8457b7dbc6025ef2d96ef832b0951d3631ff
goebers <robert.laitila@elisanet.fi>
2015-05-10 21:05:00 +0300

    Added initial project

---
+++ perse/Assets/scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (AudioSource))]
+public class Enemy : Entity {
+
+	public float expOnDeath;
+	private Player player;
+
+	void Start() {
+		player = GameObject.FindGameObjectWithTag ("Player").GetComponent<Player> ();
+	}
+
+	/*void Update(){
+		if (health <= 0) {
+		
+			GetComponent<AudioSource>().Play();
+		}
+	
+	
+	}*/
+
+
+	public override void Die ()
+	{
+		GetComponent<AudioSource>().Play();
+		player.AddExperience (expOnDeath);
+		base.Die ();
+	}
+}
---
+++ perse/Assets/scripts/Navigation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Navigation : MonoBehaviour {
+	NavMeshAgent agent;
+	public GameObject player;
+	
+	void Start () {
+		player = GameObject.Find ("Player");
+		agent = GetComponent<NavMeshAgent> ();
+	}
+	
+
+	void Update () {
+		agent.SetDestination (player.transform.position);
+	}
+}
